c194ed32916711096a25bd3a064cbd74d03abf19
Matvei98 <Matthias78@yandex.ru>
2024-10-09 18:00:57 +0300

    git init

---
+++ components/TheHeader.tsx
@@ -0,0 +1,18 @@
+import { Navigation } from './Navigation';
+
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
+
+const TheHeader = () => {
+  return (
+    <header>
+      <Navigation navLinks={navItems} />
+    </header>
+  );
+};
+
+export { TheHeader };
